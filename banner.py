4e33758fca58fee02fad20b303c71eab11d08661
Naveen Konrajankuppam Mahavishnu <Naveen94@users.noreply.github.com>
2018-08-13 14:38:31 -0400

    Update banner.py

--- banner.py
+++ banner.py
@@ -3,5 +3,6 @@ from colorama import *
 f = Figlet(font='slant')
 print f.renderText('Flash Breaker')
 init(autoreset=True)
-print Style.BRIGHT + Fore.RED + '                                        - Flash Vulnerable Methods Finder'
+print Style.BRIGHT + Fore.RED + '                                               - Flash Vulnerable Methods Finder'
+print Style.BRIGHT + Fore.WHITE + '\nCreated by' + Style.BRIGHT + Fore.CYAN + ' Naveen KM' + Style.BRIGHT + Fore.WHITE + ' and I dedicate this project to my Mentor' + Style.BRIGHT + Fore.CYAN + ' Collin Hart'
 
