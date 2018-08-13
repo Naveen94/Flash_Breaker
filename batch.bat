e2eea54e776961850439efe58b40aeab67444030
Naveen Konrajankuppam Mahavishnu <Naveen94@users.noreply.github.com>
2018-08-13 14:09:26 -0400

    Update batch.bat

--- batch.bat
+++ batch.bat
@@ -1,4 +1,4 @@
 cd decompiled
-python Hart.py
+python Flash.py
 echo "All the Findings were saved inside the Decompiled Folder - check out for <filename>_match.txt files"
 echo "Finished.. Go ahead Hacker!!"
