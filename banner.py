61f8be3a50478b809f37fea246027621643a3dd8
Naveen Konrajankuppam Mahavishnu <Naveen94@users.noreply.github.com>
2018-08-14 21:07:34 -0500

    Update banner.py

--- banner.py
+++ banner.py
@@ -4,5 +4,5 @@ f = Figlet(font='slant')
 print f.renderText('Flash Breaker')
 init(autoreset=True)
 print Style.BRIGHT + Fore.RED + '                                               - Flash Vulnerable Methods Finder'
-print Style.BRIGHT + Fore.WHITE + '\nCreated by' + Style.BRIGHT + Fore.CYAN + ' Naveen KM' + Style.BRIGHT + Fore.WHITE + ' and he dedicated to his Mentor' + Style.BRIGHT + Fore.CYAN + ' Collin Hart'
+print Style.BRIGHT + Fore.WHITE + '\nCreated by' + Style.BRIGHT + Fore.CYAN + ' Naveen KM' + Style.BRIGHT + Fore.WHITE + ' and dedicated to his Mentor' + Style.BRIGHT + Fore.CYAN + ' Collin Hart'
 
